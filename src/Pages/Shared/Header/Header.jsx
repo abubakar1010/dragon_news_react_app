5596659d041eaa131894f8454d6b3e709d6440d9
Abubakar1010 <abubakar850772@gmail.com>
2024-04-13 04:36:59 +0600

    added header

---
+++ src/Pages/Shared/Header/Header.jsx
@@ -0,0 +1,13 @@
+import logo from "../../../assets/logo.png"
+
+const Header = () => {
+    return (
+        <>
+            <div>
+                <img src={logo} alt="" />
+            </div>
+        </>
+    );
+};
+
+export default Header;
