7af7f20dd8848a5b2fec2cd681c67bd1e54ffecd
Abubakar1010 <abubakar850772@gmail.com>
2024-04-13 05:17:12 +0600

    finished header

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,10 +1,15 @@
+import moment from "moment";
 import logo from "../../../assets/logo.png"
 
 const Header = () => {
     return (
         <>
-            <div>
+            <div className=" text-center my-12">
                 <img src={logo} alt="" />
+                <div>
+                    <p className=" text-[#706F6F] text-lg mt-5 mb-4">Journalism Without Fear or Favour</p>
+                    <p className=" text-xl text-[#403F3F]">{moment().format("dddd, MMMM D, YYYY")}</p>
+                </div>
             </div>
         </>
     );
