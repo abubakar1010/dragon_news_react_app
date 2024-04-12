def690213535336690730320a24e18f440f08615
Abubakar1010 <abubakar850772@gmail.com>
2024-04-13 04:36:45 +0600

    added home page

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../Shared/Header/Header";
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <h1 className=" bg-red-500 text-5xl font-poppins">Home</h1>
+        </div>
+    );
+};
+
+export default Home
