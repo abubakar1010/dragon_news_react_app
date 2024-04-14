03e9788f3b9f47e1a6c5a6145280a3f05581ec19
Abubakar1010 <abubakar850772@gmail.com>
2024-04-14 10:23:00 +0600

    finished implement private route

--- src/Private/PrivateRoute.jsx
+++ src/Private/PrivateRoute.jsx
@@ -1,19 +1,30 @@
 import { useContext } from "react";
 import { AuthContext } from "../AuthProvider/AuthProvider";
-import { Navigate } from "react-router-dom";
-
+import { Navigate, useLocation } from "react-router-dom";
+import PropTypes from "prop-types"
+import { Spinner } from "@material-tailwind/react";
 
 const PrivateRoute = ({children}) => {
 
-    const {user} = useContext(AuthContext)
+    const {user,loading} = useContext(AuthContext)
+    const location = useLocation()
+
+    if (loading) {
+        return <div className=" flex justify-center items-center h-screen"><Spinner color="green"  className=" w-8 h-8" /></div>
+    }
 
     if (user) {
         
         return children
     }
     return (
-        <Navigate to={"/login"} ></Navigate>
+        <Navigate state={location.pathname} to={"/login"} ></Navigate>
     );
 };
+
+PrivateRoute.propTypes = {
+
+    children: PropTypes.node
+}
 
 export default PrivateRoute;
--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../Layouts/Root";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
+import DetailsNews from "../Components/DetailsNews/DetailsNews";
+import PrivateRoute from "../Private/PrivateRoute";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +14,20 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home />
+            },
+            {
+                path: "/login",
+                element: <Login />
+            },
+            {
+                path: "/register",
+                element: <Register />
+            },
+            {
+                path: "/news/:id",
+                element: <PrivateRoute>
+                    <DetailsNews />
+                </PrivateRoute>
             }
         ]
     }
